e3959578863ba3d8f916320af8e5cdd1cbb2f498
Vladimir Ryabchich <vladimir.ryabchich@gmail.com>
2016-07-25 11:02:32 +0300

    gulp & admin ui

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 var f = require('firenze');
 var SqliteAdapter = require('firenze-adapter-sqlite3');
+var config = require('./config');
 
 var Database = f.Database;
 
 var db = new Database({
   adapter: SqliteAdapter,
-  filename: 'db/data.db',
+  filename: config.sqlite.database,
   migrations: {
     table: 'z_migrations',
     directory: __dirname + '/migrations',
